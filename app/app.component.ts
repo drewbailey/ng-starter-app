188d30abd3d27a99f92a90278b46caa606cdbfd4
Drew Bailey <drewbailey5@gmail.com>
2016-05-06 11:36:17 -0600

    hero starter app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,43 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  providers: [ROUTER_PROVIDERS, HeroService],
+  directives: [ROUTER_DIRECTIVES],
+  styleUrls: ['app/app.component.css'],
+  template: `
+    <h1>{{title}}</h1>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent { }
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  }
+])
+
+export class AppComponent {
+  title :string = 'Tour of Heroes';
+}
